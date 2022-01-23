a54337f0a5d8657d2cbc20060025502d629ef01e
Rafael <rafaelmartinez.contato@gmail.com>
2022-01-23 18:17:26 -0300

    novo exercicio avaliar alunos usando map

--- sandbox.js
+++ sandbox.js
@@ -166,3 +166,82 @@ const cities = [
 const letters = cities.map ( citie => citie.slice(0, 1));
 
 // console.log(letters);
+
+//Exercicio (avaliação estudantes) - usando map
+
+const students = [
+    {
+      name: 'Tony Parker',
+      firstProject: 80,
+      secondProject: 75,
+      finalExam: 90
+    },
+    {
+      name: 'Marc Barchini',
+      firstProject: 84,
+      secondProject: 65,
+      finalExam: 65
+    },
+    {
+      name: 'Claudia Lopez',
+      firstProject: 45,
+      secondProject: 95,
+      finalExam: 99
+    },
+    {
+      name: 'Alvaro Briattore',
+      firstProject: 82,
+      secondProject: 92,
+      finalExam: 70
+    },
+    {
+      name: 'Isabel Ortega',
+      firstProject: 90,
+      secondProject: 32,
+      finalExam: 85
+    },
+    {
+      name: 'Francisco Martinez',
+      firstProject: 90,
+      secondProject: 55,
+      finalExam: 78
+    },
+    {
+      name: 'Jorge Carrillo',
+      firstProject: 83,
+      secondProject: 77,
+      finalExam: 90
+    },
+    {
+      name: 'Miguel López',
+      firstProject: 80,
+      secondProject: 75,
+      finalExam: 75
+    },
+    {
+      name: 'Carolina Perez',
+      firstProject: 85,
+      secondProject: 72,
+      finalExam: 67
+    },
+    {
+      name: 'Ruben Pardo',
+      firstProject: 89,
+      secondProject: 72,
+      finalExam: 65
+    }
+  ];
+  
+
+const finalGrades = students.map (theStudent => {
+
+    const projectsAvg = (theStudent.firstProject + theStudent.secondProject) / 2;
+    const finalGrade = theStudent.finalExam * 0.6 + projectsAvg * 0.4;
+
+    return {
+        name: theStudent.name,
+        finalGrade: Math.round(finalGrade)
+    }
+});
+
+// console.log(finalGrades);
