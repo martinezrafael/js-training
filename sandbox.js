f193023360cf8bba59c3f44ccadd7495df380472
Rafael <rafaelmartinez.contato@gmail.com>
2022-01-23 17:32:54 -0300

    adicionando novos exercicios resolvidos

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,5 @@
+//Exercício com filter (Drinking age) - Filtrar as pessoas que podem frequentar bares de acordo com a idade
+
 const people = [
   { name: "Candice", age: 25 },
   { name: "Tammy", age: 30 },
@@ -13,4 +15,88 @@ const ofDrinkingAge = people.filter((people) => {
   }
 });
 
-console.log(ofDrinkingAge);
+// console.log(ofDrinkingAge);
+
+
+//Exercicio filter (Swimming pool) - Filtrar apenas as casas que possuem piscina
+
+const places = [
+    {
+      title: "Awesome Suite 20' away from la Rambla",
+      price: 200,
+      type: 'Private Room',
+      pool: true,
+      garage: false
+    },
+    {
+      title: 'Private apartment',
+      price: 190,
+      type: 'Entire Place',
+      pool: true,
+      garage: true
+    },
+    {
+      title: 'Apartment with awesome views',
+      price: 400,
+      type: 'Entire Place',
+      pool: false,
+      garage: false
+    },
+    {
+      title: 'Apartment in la Rambla',
+      price: 150,
+      type: 'Private Room',
+      pool: false,
+      garage: true
+    },
+    {
+      title: 'Comfortable place in Barcelona´s center',
+      price: 390,
+      type: 'Entire place',
+      pool: true,
+      garage: true
+    },
+    {
+      title: 'Room near Sagrada Familia',
+      price: 170,
+      type: 'Private Room',
+      pool: false,
+      garage: false
+    },
+    {
+      title: 'Great house next to Camp Nou',
+      price: 140,
+      type: 'Entire place',
+      pool: true,
+      garage: true
+    },
+    {
+      title: 'New apartment with 2 beds',
+      price: 2000,
+      type: 'Entire place',
+      pool: false,
+      garage: true
+    },
+    {
+      title: 'Awesome Suite',
+      price: 230,
+      type: 'Private Room',
+      pool: false,
+      garage: false
+    },
+    {
+      title: "Apartment 10' from la Rambla",
+      price: 930,
+      type: 'Entire place',
+      pool: true,
+      garage: true
+    }
+  ];
+  
+const ofPool = places.filter ( place => {
+    if (place.pool) {
+        return place;
+    }
+});
+
+// console.log(ofPool);
